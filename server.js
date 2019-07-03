202da6c9c459f0a12e03a89e3d93aa9ca41a18b0
bhosun <bhorsun@yahoo.com>
2019-07-02 23:26:05 -0800

    Created the initial phase for the Comment Routes

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const express = require("express");
-const fetch   = require("node-fetch");
+const express       = require("express");
+const fetch         = require("node-fetch");
+const Pool          = require("pg").Pool;
+const bodyParser    = require("body-parser");
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'codechallenge',
+    password: 'bosunbosun71',
+    port: 5432
+});
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// route to Get the EPISODES
 app.get("/episodes", (req, res) => {
     fetch("https://rickandmortyapi.com/api/episode")
     .then(res => res.json())
@@ -18,6 +31,31 @@ app.get("/episodes", (req, res) => {
     .catch((err) => {
         console.log(`you have an error: ${err.message}`)
     })
+});
+
+// ADDING AND LISTING COMMENTS FOR AN EPISODE
+app.get("/comments", (req, res) => {
+    pool.query('SELECT * FROM comment', (err, data) => {
+        if(err) {
+            throw err;
+        }
+        res.status(200).json(data.rows)
+    });
+});
+
+app.post("/comments", (req, res) => {
+    const { comment } = req.body;
+    if(!comment) {
+        res.status(400).json("bobo yii oti gbe gbagii");
+    }
+
+    pool.query('INSERT INTO comment (comment) VALUES ($1)', [comment], (err, data) => {
+        if(err) {
+            throw err
+        }
+        console.log(req.body);
+        res.status(201).json("Successfully added!!");
+    });
 });
 
 app.listen(3000, () => {
